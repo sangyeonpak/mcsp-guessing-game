1bef39ddb91638eee4818652b0856595165fb017
Sangyeon Pak <sangyeonpak1994@gmail.com>
2022-11-08 13:22:40 -0500

    No comments version

--- app.js
+++ app.js
@@ -1,62 +1,45 @@
-//============ OBJECTS THAT KEEPS HIGH SCORES ==========
 let starPlayers = {};
-/* starPlayers has to look like
-starPlayers = {
-  Sang: 3
-}*/
 
-//window.localStorage?
+function play(){
 
+  const secretNumber = Math.floor(Math.random() * 10 + 1);
+  console.log("Cheater! The answer is " + secretNumber);
+  var trial = 1;
+  let prevNumber = [];
 
-
-//============= PLAY =============\\
-function play(){ // game start
-
-  // IMPORTANT VARIABLES THAT RESET EACH TIME YOU PLAY
-  const secretNumber = Math.floor(Math.random() * 10 + 1); // random number generator per game
-  console.log("Cheater! The answer is " + secretNumber); // console log to make myself as the dev easier
-  var trial = 1; // my number of tries starts at 1
-  let prevNumber = []; // empty array of all the inputs the player put in so far
-
-  // NAME PROMPT
-  let playerName = prompt("Hi, welcome to Guess a Number! What is your name?"); // name prompt
-  if (playerName === null){ // if they press x or just press esc, instead of reading the input as nothing written or 0, it quits the dialogue
-    alert("Goodbye!"); // goodbye message when the program quits, doesn't prompt or alert anything else.
+  let playerName = prompt("Hi, welcome to Guess a Number! What is your name?");
+  if (playerName === null){
+    alert("Goodbye!");
   } else {
 
-    //======== LOOP TO KEEP GAME GOING UNTIL YOU GET THE ANSWER =======================
-    while(true){ // This condition is always gonna be true. This loop will keep on happening until the code is broken with "break". So until the person gets it correct, it keeps going. Thanks Jeremy for the while(true)
-
-      let guessStr = prompt("Alright, " + playerName + ", guess a number from 1 to 10. \n     This is what you have guessed so far: "+ `${prevNumber}` + "\n\n SCOREBOARD: \n" + JSON.stringify(starPlayers, null, 4).replace(/"/g,'').replace('{','').replace('}','').replace(/,/g,'')); // this cleans up the scoreboard
-      if (guessStr === null){ // program quits with ESC or exit
+    while(true){
+      let guessStr = prompt("Alright, " + playerName + ", guess a number from 1 to 10. \n     This is what you have guessed so far: "+ `${prevNumber}` + "\n\n SCOREBOARD: \n" + JSON.stringify(starPlayers, null, 4).replace(/"/g,'').replace('{','').replace('}','').replace(/,/g,''));
+      if (guessStr === null){
         alert("Goodbye!");
         break;
       }
-      let guessNum = Number(guessStr); // prompt always stores data as a string, so gotta convert
+      let guessNum = Number(guessStr);
 
-      //CORRECT GUESS
       if (guessNum === secretNumber){
         alert("That is correct, " + playerName + "! It took you " + trial + " time(s).");
-        if (starPlayers[playerName] === undefined){ // creating an object with the playerName if there is none yet
+        if (starPlayers[playerName] === undefined){
           starPlayers[playerName] = {};
           starPlayers[playerName] = trial;
         } else{
-          if (starPlayers[playerName] > trial){ // same name, new score is lower? set highscore (low rather lol) to the new score
+          if (starPlayers[playerName] > trial){
             alert(playerName + ', you also scored better with ' + (starPlayers[playerName] - trial) + ' fewer guesses. Good job!')
             starPlayers[playerName] = trial;
           }
         }
-        let yesOrNo = confirm('Would you like to play again, ' + playerName + '? Please click "OK" if you want to play again.'); //Thanks Lance for the button idea
+        let yesOrNo = confirm('Would you like to play again, ' + playerName + '? Please click "OK" if you want to play again.');
         if (yesOrNo === null || yesOrNo === false){
           alert('Goodbye! Thank you for playing.');
         } else{
           play();
         }
         break;
-
-      //INCORRECT GUESSES
       } else if(guessNum <= 0 || guessNum > 10){
-        prevNumber.push(guessStr); // pushing the string, not the converted number, in case they add words or something and it'll display that
+        prevNumber.push(guessStr);
         alert("Please enter a number between 1 and 10, " + playerName + ". You guessed ", trial, " time(s) so far.");
       } else if(guessNum > secretNumber){
         prevNumber.push(guessStr);
